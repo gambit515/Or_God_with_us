5894d6d03d9c926446dbc1e30ea9786ee7987c78
gambit515 <122382344+gambit515@users.noreply.github.com>
2023-04-07 16:28:27 +0700

    add DB

--- SpecialistSyte/main/models.py
+++ SpecialistSyte/main/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
+class Users(models.Model):
+    Login = models.CharField(max_length=50)
+    Password = models.CharField(max_length=50)
+    Email = models.EmailField(max_length=254)
     SerName = models.CharField(max_length=50)
     Name = models.CharField(max_length=50)
     Patronymic = models.CharField(max_length=50)
     UserType = models.CharField(max_length=50)
-    Login = models.CharField(max_length=50)
-    Email = models.EmailField(max_length=254)
-    Password = models.EmailField(max_length=254)
+    Photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    Time_create = models.DateTimeField(auto_now_add=True)
+
 
