8231f14dacd4ad1aabd61f891c0a9437ccff8ce2
Alexander Pachomov <paxomov.2004@list.ru>
2023-04-11 13:40:39 +0700

    проверка

--- SpecialistSyte/main/views.py
+++ SpecialistSyte/main/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .models import Anketa
 
 def mainsheet(request):
-    return render(request, 'main/mainsheet.html')
+    anketas =  Anketa.objects.all()
+    return render(request, 'main/mainsheet.html',{'anketas':anketas})
 
 def registration(request):
     return render(request, 'main/registeration.html')
+
+def startsheet(request):
+    return render(request, 'main/startsheet.html')
