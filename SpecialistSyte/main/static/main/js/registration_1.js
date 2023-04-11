12f6037b90938ca44bf0773e3bb509841b077028
Alexander Pachomov <paxomov.2004@list.ru>
2023-04-11 20:23:38 +0700

    макро изменения

--- SpecialistSyte/main/static/main/js/registration_1.js
+++ SpecialistSyte/main/static/main/js/registration_1.js
@@ -1,7 +1,7 @@
 
 //объявляем ссылки на получаемые данные
 const myForm = document.querySelector('#form-group');
-const UserInput = document.querySelector('#user');
+const UserInput = document.querySelector('#user_psevdomin');
 const passwordInput = document.querySelector('#password');
 const DpasswordInput = document.querySelector('#Dpassword');
 const MailInput = document.querySelector('#mail');
@@ -9,6 +9,16 @@ const RadiosInput1 = document.querySelector('#exampleRadios1');
 const RadiosInput2 = document.querySelector('#exampleRadios2');
 const BirthDayInput = document.querySelector('#BirthDay');
 const msg = document.querySelector('.msg');
+
+
+console.log(UserInput);
+console.log(passwordInput);
+console.log(DpasswordInput);
+console.log(MailInput);
+console.log(RadiosInput1);
+console.log(RadiosInput2);
+console.log(BirthDayInput);
+console.log(msg);
 
 
 //Общий код проверки всех полей:
@@ -19,7 +29,7 @@ myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e){
     
-   //e.preventDefault();
+  // e.preventDefault();
 
     if(UserInput.value === ""){
         e.preventDefault();
