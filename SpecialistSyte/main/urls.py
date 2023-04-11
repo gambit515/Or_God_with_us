822b1255445fd3c6cd416109aecb8b77b6f95eab
gambit515 <122382344+gambit515@users.noreply.github.com>
2023-04-11 14:26:17 +0700

    починил починенное

--- SpecialistSyte/main/urls.py
+++ SpecialistSyte/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.startsheet(), name = 'start'),
+    path('',views.startsheet, name = 'start'),
     path('registration/',views.registration, name = 'reg'),
-    path('main/',views.mainsheet(), name = 'main'),
+    path('main/',views.mainsheet, name = 'main'),
 ]
