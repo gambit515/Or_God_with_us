6986c56bf5c30f080becf75f42b36635cd0aabd5
scontt <deelog1233223@gmail.com>
2023-10-01 08:22:11 +0700

    12322

--- SpecialistSyte/main/static/main/js/main.js
+++ SpecialistSyte/main/static/main/js/main.js
@@ -1,8 +1,7 @@
 let wrapper = document.querySelector('.wrapper')
 let vacancy = document.querySelectorAll(".vacancies-item");
 let isExpanded = false;
-        let dynamicStyles = null;
-
+let dynamicStyles = null;
         const itemHeight = vacancy[0].offsetHeight;
 
         vacancy.forEach(i => {
@@ -17,30 +16,20 @@ let isExpanded = false;
                     document.querySelectorAll( ".open-vacan").forEach(elem => {
                         elem.style.filter = ('blur(20px)')
                     });
+                    document.querySelectorAll( ".search").forEach(elem => {
+                        elem.style.filter = ('blur(20px)')
+                    });
                     i.style.filter = ('blur(0)');
                     
-                    addAnimation(`@keyframes expand {
-                        from {
-                            height: ${itemHeight}px;
-                        }
-                        to {
-                            height: 400px;
-                        }
-                    }`)
                     i.style.animationName = 'expand'
-                    i.style.height = '400px';
+                    i.style.height = 'auto';
                     i.style.transform = 'scale(1.5)'
                     i.style.backgroundColor = 'var(--blue-color)'
 
-                    let button = document.createElement("button");
-                    let buttonText = document.createTextNode("Откликнуться");
-                    button.appendChild(buttonText);
-                    let lastItem = i.lastChild.nextSibling;
-                    button.classList.add('btn');
-                    button.classList.add('response-btn');
-                    i.insertBefore(button, lastItem);
-
-                    button.addEventListener("click", responseHandler);
+                    let more = document.querySelectorAll('.vacancies-item > .more');
+                    i.querySelectorAll('.more').forEach(item => {
+                        item.style.visibility = 'visible'
+                    })
 
                     isExpanded = true;
                 }
@@ -48,31 +37,21 @@ let isExpanded = false;
                     document.querySelectorAll('*').forEach(elem => {
                         elem.style.filter = 'blur(0)'
                     });
-                    addAnimation(`@keyframes reduce {
-                        from {
-                            height: 400px;
-                        }
-                        to {
-                            height: ${itemHeight}px;
-                        }
-                    }`)
+                    
                     i.style.animationName = 'reduce'
                     i.style.height = 'fit-content'
                     i.style.transform = 'scale(1)'
                     i.style.backgroundColor = 'var(--blue-color-op)'
 
-                    const button = document.querySelector("button")
-                    i.removeChild(button)
+                    i.querySelectorAll('.more').forEach(item => {
+                        item.style.visibility = 'collapse'
+                    })
 
                     isExpanded = false;
                 }
             });
             
         })
-
-        function responseHandler() {
-            window.open('./response.html', "_self");
-        }
 
         function addAnimation(body) {
             if (!dynamicStyles) {
